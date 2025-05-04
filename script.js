a2447dfde3cce0576ac4b827a3c310b8768c09ca
Abhilash Kumar Mehta <145607994+ABHILASH-KUMAR-MEHTA@users.noreply.github.com>
2025-05-04 10:10:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 function highlight() {
     //Write your code here
+ const strongTags = document.querySelectorAll('strong');
+  strongTags.forEach(tag => {
+    tag.style.color = 'rgb(0, 128, 0)'; // green
+  });
+}
 
+function return_normal() {
+  const strongTags = document.querySelectorAll('strong');
+  strongTags.forEach(tag => {
+    tag.style.color = 'rgb(0, 0, 0)'; // black
+  });
 
 }
 
